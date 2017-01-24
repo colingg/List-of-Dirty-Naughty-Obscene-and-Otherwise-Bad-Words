a21d46beca1e689cbd1646157ab5b4555ec71104
colingg <colingg@users.noreply.github.com>
2017-01-24 19:32:36 +0100

    Load & read in unicode.

--- tools/sort.ps1
+++ tools/sort.ps1
@@ -6,6 +6,6 @@ foreach ($file in $Files)
     $filename = $file.ToString();
     if($filename -eq "tools" -or $filename -eq "LICENSE" -or $filename -eq "README.md" -or $filename -eq "USERS.md"){continue}
     Write-Host " --- Sorting ----  " $file  
-    $content = Get-Content $file.FullName | sort | get-unique
-    $content | Out-File $file.FullName
+    $content = Get-Content $file.FullName -encoding Unicode | sort | get-unique
+    $content | Out-File $file.FullName -encoding Unicode
 }
