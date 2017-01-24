7ca07391c13d1ff754b9d0b3a151d782887aeb44
colingg <colingg@users.noreply.github.com>
2017-01-24 19:26:03 +0100

    Tool to sort & cleanup the lists

---
+++ tools/sort.ps1
@@ -0,0 +1,11 @@
+$RootPath = Split-Path $PSScriptRoot
+$Files = Get-ChildItem $RootPath
+
+foreach ($file in $Files)
+{
+    $filename = $file.ToString();
+    if($filename -eq "tools" -or $filename -eq "LICENSE" -or $filename -eq "README.md" -or $filename -eq "USERS.md"){continue}
+    Write-Host " --- Sorting ----  " $file  
+    $content = Get-Content $file.FullName | sort | get-unique
+    $content | Out-File $file.FullName
+}
