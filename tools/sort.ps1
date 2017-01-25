d42fecdfb1ba18fed2dd83a1f803f18161fb5c55
colingg <colingg@users.noreply.github.com>
2017-01-25 01:13:02 +0100

    Reading & Writing with UTF8 seems sufficient for non ASCII

--- tools/sort.ps1
+++ tools/sort.ps1
@@ -6,15 +6,7 @@ foreach ($file in $Files)
     $filename = $file.ToString();
     if($filename -eq "tools" -or $filename -eq "LICENSE" -or $filename -eq "README.md" -or $filename -eq "USERS.md"){continue}
     #manual skips on non ASCII languages.
-    if($filename -eq "ar"){continue}
-    if($filename -eq "emoji"){continue}
-    if($filename -eq "fa"){continue}
-    if($filename -eq "ja"){continue}
-    if($filename -eq "ko"){continue}
-    if($filename -eq "ru"){continue} #this might actually work, needs testing.
-    if($filename -eq "th"){continue}
-    if($filename -eq "zh"){continue}
     Write-Host " --- Sorting ----  " $file  
-    $content = Get-Content $file.FullName -encoding Unicode | sort | get-unique
-    $content | Out-File $file.FullName -encoding Unicode
+    $content = Get-Content $file.FullName -encoding UTF8 | sort | get-unique
+    $content | Out-File $file.FullName -encoding UTF8
 }
